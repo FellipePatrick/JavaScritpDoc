fc752d7cc6bd1fe4fb8e909d1f8810e6d9b3021a
Fellipe Patrick Lima de Brito <3254783@ifrn.local>
2022-12-28 12:36:16 -0300

    adicionando codigo

--- modulo1/script.js
+++ modulo1/script.js
@@ -1,1 +1,28 @@
-alert("Ola Mundo");
+console.log("------------------   WHILE   ------------------------------");
+
+n = 0;
+x = 0;
+while (n < 4) {
+    n++;
+    x += n; 
+    console.log(x);
+}
+
+console.log("----------------      DO WHILE       ------------------------");
+
+i = 0;
+
+do {
+    i = i +1;
+    console.log(i);
+} while (i < 5);
+
+console.log("-----------------          FOR        -----------------------");
+
+
+var cont;
+
+for (cont = 0; cont < 1; cont++)
+ {
+    console.log("Fellipe Patrick");
+ }
---
+++ modulo1/script2.js
@@ -0,0 +1,13 @@
+for (i = 0; i <= 5; i++) {
+    
+    document.write("Bom dia!" + i + "<br> <br>");
+}
+
+y = 9;
+
+x = 10;
+
+y = x +1;
+
+x = y + 1;
+
