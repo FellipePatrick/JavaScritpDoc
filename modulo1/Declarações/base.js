196020702fe4ec8e5cf707e94be63e5cce443592
fellipepatrick <fellipe.patrick@escolar.ifrn.edu.br>
2023-05-23 21:39:49 -0300

    primeiro modulo

---
+++ modulo1/Declarações/base.js
@@ -0,0 +1,9 @@
+//Formas de declarações no JS
+
+let a = 5, b = 7, c = 8;
+
+let pessoa = "Dimitri";
+
+function minhafuncao(){
+    let a;
+}
---
+++ modulo1/Var Let Const/base.js
@@ -0,0 +1,15 @@
+// var -> a variavel pode ser redeclaravel
+
+// let -> a variavel não pode redeclaravel 
+
+// const -> não varia, é uma constante 
+
+var nome, sobrenome, nomeCompleto;
+
+nome = "Fellipe ";
+
+sobrenome = "Patrick";
+
+nomeCompleto = nome + sobrenome;
+
+document.getElementById("texto").innerHTML = nomeCompleto;
